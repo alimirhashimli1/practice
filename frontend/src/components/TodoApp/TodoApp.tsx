c1ea97d3195dfd6cc496c1bfc6ebf1ecfca9dcaf
alimirhashimli1 <alimirhashimli@gmail.com>
2025-05-13 02:15:40 +0200

    modularized the app

--- frontend/src/components/TodoApp/TodoApp.tsx
+++ frontend/src/components/TodoApp/TodoApp.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { Todo } from './types';
-import { addTodo, deleteTodo, saveTodo, sortTodo, toggleTodoCompleted } from '../../features/todo/todosSlice';
+import { addTodo,  sortTodo } from '../../features/todo/todosSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../store';
+import TodoItem from './TodoItem';
 
 const TodoApp: React.FC = () => {
   const [error, setError] = useState<string>('');
@@ -85,95 +86,19 @@ const TodoApp: React.FC = () => {
 
       <ul className="mt-6 space-y-4">
         {todos.map((todo) => (
-          <li
-            key={todo.id}
-            className={`p-4 border border-gray-200 rounded-md shadow-sm transition-colors ${
-              todo.completed ? 'bg-green-100' : 'bg-gray-50'
-            }`}
-            onClick={() => dispatch(toggleTodoCompleted(todo.id))}
-          >
-            {editTodoId === todo.id ? (
-              <div className="flex flex-col sm:flex-row sm:items-center gap-2">
-                <input
-                  type="text"
-                  value={editedText}
-                  onClick={(e) => e.stopPropagation()}
-                  onChange={(e) => setEditedText(e.target.value)}
-                  className="flex-1 p-2 border rounded-md w-full sm:w-auto"
-                />
-                <select
-                  value={editedCategory}
-                  onClick={(e) => e.stopPropagation()}
-                  onChange={(e) => setEditedCategory(e.target.value)}
-                  className="p-2 border rounded-md w-full sm:w-auto"
-                >
-                  <option disabled value="">Select a Category</option>
-                  <option value="Learning">Learning</option>
-                  <option value="Personal">Personal</option>
-                  <option value="Hobbies">Hobbies</option>
-                </select>
-                <button
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    dispatch(
-                      saveTodo({
-                        id: todo.id,
-                        todo: editedText,
-                        category: editedCategory,
-                        completed: todo.completed,
-                      })
-                    );
-                    setEditTodoId(null);
-                    setEditedText('');
-                    setEditedCategory('');
-                    setError('');
-                  }}
-                  className={`px-4 py-2 text-sm rounded-md border transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 border-green-600 text-green-600 hover:bg-green-100`}
-                >
-                  Save
-                </button>
-              </div>
-            ) : (
-              <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-2">
-                <div>
-                  <p
-                    className={`font-semibold ${
-                      todo.completed ? 'line-through text-gray-500 opacity-70' : 'text-gray-800'
-                    }`}
-                  >
-                    {todo.todo}
-                  </p>
-                  <p
-                    className={`text-sm ${
-                      todo.completed ? 'line-through text-gray-400 opacity-70' : 'text-gray-500'
-                    }`}
-                  >
-                    {todo.category}
-                  </p>
-                </div>
-                <div className="flex flex-wrap gap-2">
-                  <button
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      handleEdit(todo.id, todo.todo, todo.category);
-                    }}
-                    className={`px-4 py-2 text-sm rounded-md border transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 border-blue-600 text-blue-600 hover:bg-blue-100`}
-                  >
-                    Edit
-                  </button>
-                  <button
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      dispatch(deleteTodo(todo.id));
-                    }}
-                    className={`px-4 py-2 text-sm rounded-md border transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 border-red-600 text-red-600 hover:bg-red-100`}
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
-            )}
-          </li>
+         <TodoItem
+         todo={todo}
+         editTodoId={editTodoId}
+         editedText={editedText}
+         editedCategory={editedCategory}
+         setEditedText={setEditedText}
+         setEditTodoId={setEditTodoId}
+         setError={setError}
+         setEditedCategory={setEditedCategory}
+         handleEdit={handleEdit}
+        
+          
+         />
         ))}
       </ul>
       <button  className="px-4 py-2 my-2 w-full text-sm rounded-md border transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 border-violet-600 text-violet-600 hover:bg-violet-100" onClick={() => dispatch(sortTodo())}>Sort Todos</button>
--- frontend/src/components/TodoApp/TodoItem.tsx
+++ frontend/src/components/TodoApp/TodoItem.tsx
@@ -1,43 +1,117 @@
+import { useDispatch } from "react-redux"
+import { deleteTodo, saveTodo, toggleTodoCompleted } from "../../features/todo/todosSlice"
+
 interface TodoItemsProps {
     todo: {
-      task: string,
+      todo: string,
       id: string,
+      category: string,
       completed: boolean
     },
-    handleEdit: (todoId: string, todoTask: string) => void;
-    handleDelete: (index: string) => void;
-    handleSave: (id: string) => void;
-    editId: boolean;
-    editTask: string;
-    setEditTask: (value: string) => void
-    handleToggle: (index: string) => void;
+    editTodoId: string | null,
+    editedText: string,
+    editedCategory: string,
+      setEditedText: (text: string) => void
+      setEditedCategory: (category: string) => void
+      setEditTodoId: (id: string | null) => void
+      setError: (error: string) => void
+      handleEdit: (todoId: string, todoText: string, todoCategory: string) => void
 }
 
-const TodoItem: React.FC<TodoItemsProps> = ({todo, handleEdit, handleDelete, handleSave, editId, editTask, setEditTask, handleToggle}) => {
+const TodoItem: React.FC<TodoItemsProps> = ({todo, editTodoId, editedText, editedCategory, setEditedText, setEditedCategory, setEditTodoId, setError, handleEdit}) => {
+  const dispatch = useDispatch();
 
 
   return (
-        <li className="flex gap-2 justify-between items-center m-2" key={todo.id}>
-              {
-                editId ? (
-                  <div className="flex justify-between w-full gap-2">
-                  <input className="flex-1 p-2 w-24" type="text" value={editTask} onChange={(e) => setEditTask(e.target.value)}/>
-                  <button className="ml-2  w-24 bg-green-500 hover:bg-green-700 transition p-2" onClick={() => handleSave(todo.id)}>Save</button>
-                  </div>
-
-                
-                ) :
-                
-                (
-                  <div className="flex justify-between w-full gap-2">
-                  <p className={`cursor-pointer w-24 ${todo.completed ? "line-through" : ""}`} onClick={() => handleToggle(todo.id)}>{todo.task}</p>
-                  <button className="ml-2  w-24 bg-orange-500 hover:bg-orange-700 transition p-2" onClick={() => handleEdit(todo.id, todo.task)}>Edit</button>
-                  </div>
-
-                )
-              }
-              <button className="ml-2  w-24 bg-red-500 hover:bg-red-700 transition p-2" onClick={() => handleDelete(todo.id)}>Delete</button>
-            </li>
+        <li
+            key={todo.id}
+            className={`p-4 border border-gray-200 rounded-md shadow-sm transition-colors ${
+              todo.completed ? 'bg-green-100' : 'bg-gray-50'
+            }`}
+            onClick={() => dispatch(toggleTodoCompleted(todo.id))}
+          >
+            {editTodoId === todo.id ? (
+              <div className="flex flex-col sm:flex-row sm:items-center gap-2">
+                <input
+                  type="text"
+                  value={editedText}
+                  onClick={(e) => e.stopPropagation()}
+                  onChange={(e) => setEditedText(e.target.value)}
+                  className="flex-1 p-2 border rounded-md w-full sm:w-auto"
+                />
+                <select
+                  value={editedCategory}
+                  onClick={(e) => e.stopPropagation()}
+                  onChange={(e) => setEditedCategory(e.target.value)}
+                  className="p-2 border rounded-md w-full sm:w-auto"
+                >
+                  <option disabled value="">Select a Category</option>
+                  <option value="Learning">Learning</option>
+                  <option value="Personal">Personal</option>
+                  <option value="Hobbies">Hobbies</option>
+                </select>
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    dispatch(
+                      saveTodo({
+                        id: todo.id,
+                        todo: editedText,
+                        category: editedCategory,
+                        completed: todo.completed,
+                      })
+                    );
+                    setEditTodoId(null);
+                    setEditedText('');
+                    setEditedCategory('');
+                    setError('');
+                  }}
+                  className={`px-4 py-2 text-sm rounded-md border transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 border-green-600 text-green-600 hover:bg-green-100`}
+                >
+                  Save
+                </button>
+              </div>
+            ) : (
+              <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-2">
+                <div>
+                  <p
+                    className={`font-semibold ${
+                      todo.completed ? 'line-through text-gray-500 opacity-70' : 'text-gray-800'
+                    }`}
+                  >
+                    {todo.todo}
+                  </p>
+                  <p
+                    className={`text-sm ${
+                      todo.completed ? 'line-through text-gray-400 opacity-70' : 'text-gray-500'
+                    }`}
+                  >
+                    {todo.category}
+                  </p>
+                </div>
+                <div className="flex flex-wrap gap-2">
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleEdit(todo.id, todo.todo, todo.category);
+                    }}
+                    className={`px-4 py-2 text-sm rounded-md border transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 border-blue-600 text-blue-600 hover:bg-blue-100`}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      dispatch(deleteTodo(todo.id));
+                    }}
+                    className={`px-4 py-2 text-sm rounded-md border transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 border-red-600 text-red-600 hover:bg-red-100`}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            )}
+          </li>
   )
 }
 
