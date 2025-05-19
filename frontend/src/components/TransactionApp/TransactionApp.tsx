fbc0882d5f6d16792a96beb2d6c051061f5102b8
alimirhashimli1 <alimirhashimli@gmail.com>
2025-05-20 01:39:59 +0200

    added transaction app with dispatch

--- frontend/src/components/TransactionApp/TransactionApp.tsx
+++ frontend/src/components/TransactionApp/TransactionApp.tsx
@@ -1,76 +1,27 @@
-// components/TransactionApp/index.tsx
-import React, { useMemo } from "react";
-import TransactionForm from "./TransactionForm";
-import TransactionList from "./TransactionList";
-import { useSelector, useDispatch } from "react-redux";
-import { RootState } from "../../store";
-import { Transaction } from "./types";
-import {
-  addTransaction,
-  deleteTransaction,
-  editTransaction,
-  sortTransactions
-} from "../../features/transactions/transactionsSlice";
+import React from 'react'
 
-const TransactionApp: React.FC = () => {
-  const [selectedTransaction, setSelectedTransaction] = React.useState<Transaction | null>(null);
-  const dispatch = useDispatch();
-  const transactions = useSelector((state: RootState) => state.transactions.transactions);
 
-  const transactionSum = useMemo(() => {
-    return transactions.reduce((acc, curr) => {
-      return curr.category === "income" ? acc + curr.amount : acc - curr.amount;
-    }, 0);
-  }, [transactions]);
-
-  const handleAddTransaction = (title: string, amount: number, category: string) => {
-    const newTransaction: Transaction = {
-      title,
-      amount,
-      category,
-      date: new Date().toLocaleDateString(),
-    };
-  
-    if (selectedTransaction) {
-      dispatch(editTransaction(newTransaction));
-      setSelectedTransaction(null); 
-    } else {
-      dispatch(addTransaction(newTransaction));
-    }
-  };
-  
-
-  const handleEditTransaction = (t: Transaction) => {
-    setSelectedTransaction(t);
-  };
-  
-  const handleDelete = (title: string) => {
-    dispatch(deleteTransaction(title));
-  };
-
-  const handleSort = () => {
-    dispatch(sortTransactions());
-  };
-
+function TransactionApp() {
   return (
-    <div className="min-h-screen bg-gray-50 py-8 px-4 flex justify-center items-start">
-    <div className="w-full max-w-2xl bg-white shadow-xl rounded-2xl p-6 space-y-6">
-      <h1 className="text-2xl font-bold text-gray-800 text-center">💸 Transaction Tracker</h1>
-      <TransactionForm onAdd={handleAddTransaction} selectedTransaction={selectedTransaction}/>
-      
-      <div className="border-t pt-4">
-     
-      <TransactionList
-        transactions={transactions}
-        onEdit={handleEditTransaction}
-        onDelete={handleDelete}
-        onSort={handleSort}
-        transactionSum={transactionSum}
-      />
-     </div>
+    <form action="">
+      <div>
+      <label htmlFor="">Title</label>
+      <input type="text" placeholder='Title' />
       </div>
-    </div>
-  );
-};
+      <div>
+      <label htmlFor="">Amount</label>
+      <input type="number" placeholder='Amount' />
+      </div>
+      <div>
+      <label htmlFor="">Category</label>
+      <select name="" id="">
+        <option disabled value="">Select category</option>
+        <option value="">Income</option>
+        <option value="">Expense</option>
+      </select>
+      </div>
+    </form>
+  )
+}
 
-export default TransactionApp;
+export default TransactionApp
